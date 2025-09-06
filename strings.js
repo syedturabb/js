f4d448e4b8786cc8695d0bcb69d7b94872aa6cd2
syedturabb <turabhaiderpk@gmail.com>
2025-09-05 23:59:52 -0700

    4th commit

---
+++ strings.js
@@ -0,0 +1,22 @@
+const name = "turab"
+const repoCount = 5
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}` );
+
+const na = "ali"
+console.log(na.length);  //3
+console.log(na.toUpperCase);  //ALI
+console.log(na.charAt(2));  // i
+
+
+const newstring = na.substring(0,2)  //al
+
+const anotherString = na.slice(-4,2);
+
+const string1 = "  turab    "
+console.log(string1.trim);
+
+
+const url ="http://turab/haider@3.com"
+
+console.log(url.replace('@3','-'));
