0e13ab9a21f8f6e954a6511bf3b293620ef92477
syedturabb <turabhaiderpk@gmail.com>
2025-09-05 23:31:22 -0700

    datatypes summary

---
+++ datatypes-summary.js
@@ -0,0 +1,30 @@
+//Datatypes    1. Primitive (call by value)      2. Non-Primitive (call by refernece)
+
+// primitive 7 types : string , number , boolean, null, undefined, symbol, BigInt
+
+
+const score = 100;
+const score1 = 100.3
+
+const isLogged = true
+const outsideTemp = null
+let userEmail= undefined 
+
+const id = Symbol('123')
+const bigNum = 234567888776652n
+
+
+
+// reference (non primitive) : Arrays , objects , functions   //typeof functions
+
+const hero = [superman, spiderman, thor];
+
+let obj = {
+    name:'t',
+    age : 20
+}
+
+const myFunc = function(){
+     console.log("hello world");     
+}
+
