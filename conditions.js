66835b830dc354d30c1db828f160d7e69bfe9353
syedturabb <turabhaiderpk@gmail.com>
2025-09-09 01:12:21 -0700

    conditions in js

---
+++ conditions.js
@@ -0,0 +1,50 @@
+let num1 = 5
+let num2 = 10
+
+if(num1 > num2){
+    console.log("num1 is greate rthan num2");
+    
+}
+else{
+    console.log("num2 is greater than num1");
+    
+}
+
+let username= "chai"
+let anotherusername="chai"
+if(username== anotherusername){
+    console.log("pick another username");
+
+}
+let score ="44"
+
+if(typeof score ===Number){
+    console.log("it is a number");
+    
+}else{
+    console.log("not a number");
+    
+}
+
+
+let isTeaReady=true    // true => 1  , false =>0
+if(isTeaReady){
+    console.log("Tea is ready");
+}
+else{
+    console.log("not ready ");   
+    
+}
+
+let items = []
+
+console.log(items.length);
+
+if(items.length===0){
+    console.log("Array is empty");
+    
+}
+else{
+    console.log("array is not empty");
+    
+}
