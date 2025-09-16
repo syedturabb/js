beb7d26b1da9057cbf3f28da33d420f1b0b3380d
syedturabb <turabhaiderpk@gmail.com>
2025-09-16 00:47:19 -0700

    intro  of func

--- functions.js
+++ functions.js
@@ -8,8 +8,31 @@ name();  // functiion
 name //reference
 
 function sum(num1,num2){
-    console.log(num1+num2);
+    // let result =num1+num2
+    // return result;
+    return num1+num2;
 }
 
 sum(3,10);
+
+const result = sum(3,111)
+console.log("Result: ",result);
+
+
+
+function loginInUser(username){
+    if(username == undefined){
+        console.log("please enter any name");
+        return;
+    }
+    return `${username} just logged in`
+}
+console.log(loginInUser("Turab"));
+
+//if nothing is passed it outputs undefined
+
+
+
+
+
 
