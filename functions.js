c510352d62f682e65dc69402f9eb8dfc0cd3175d
syedturabb <turabhaiderpk@gmail.com>
2025-09-16 00:38:58 -0700

    functions

--- functions.js
+++ functions.js
@@ -1,1 +1,15 @@
 //functions in js 
+function name(){
+    console.log("Turab");
+    
+}
+
+name();  // functiion
+name //reference
+
+function sum(num1,num2){
+    console.log(num1+num2);
+}
+
+sum(3,10);
+
