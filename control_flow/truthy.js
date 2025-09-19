96681e5400f8f52811f034b9b1ee1379195ad76c
syedturabb <turabhaiderpk@gmail.com>
2025-09-19 02:40:08 -0700

    truthy & falsy

--- control_flow/truthy.js
+++ control_flow/truthy.js
@@ -8,3 +8,17 @@ else{
     console.log("no email");
     
 }
+
+//falsy value :
+//false , 0 , -0 , BigInt 0n , "" , null, undefined , NaN
+
+
+// truthy valuse :
+// "0" , 'false' , " " , [] , {}, function() {}
+
+const emptyobj = {}
+
+if( Object.keys(emptyobj).length === 0){
+    console.log(("object is empty"));
+    
+}
