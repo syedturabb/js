bbbf83bd44ab80fe6724def5e9fda31f00b2b442
syedturabb <turabhaiderpk@gmail.com>
2025-09-19 02:45:48 -0700

    nullish coelsing op

--- control_flow/truthy.js
+++ control_flow/truthy.js
@@ -22,3 +22,12 @@ if( Object.keys(emptyobj).length === 0){
     console.log(("object is empty"));
     
 }
+
+//nullish coalescing operator ?? null : undefined
+
+let val1 ;
+val1= 5 ?? 10   // 5
+val1 = null ?? 10 // 10
+val1 = undefined ?? 15 // 15
+val1 = null ?? 10 ?? 20   // 10 {first comes}
+
