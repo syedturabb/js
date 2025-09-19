2da58a8f24b2c679de39a36ad93389cda1ee155f
syedturabb <turabhaiderpk@gmail.com>
2025-09-19 02:26:45 -0700

    control flow

--- control_flow/one.js
+++ control_flow/one.js
@@ -9,4 +9,33 @@ else{
 
 }
 
-// < , > , <= , >= , == , != 
+// < , > , <= , >= , == , != , === (checks for type as well)  , !==
+
+// let  per = 67;
+// if(per>=  90 && per < 100 ){
+//     console.log("A grade");
+    
+// }
+// else if(per < 90 && per >= 80 ){
+//     console.log("B grade ");
+    
+// }
+// else{
+//     console.log("F Grade");
+    
+// }
+
+const balance = 900
+const userLoggedIn = false
+
+if( userLoggedIn == true && balance > 0){   //multiple consitions check
+
+    console.log("Allow to shop ");
+    
+}
+
+// &&    : all must be true
+// ||    : atleast one must be true
+
+
+
