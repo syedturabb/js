79696427affd5b7b44e0da09329b2d887bed6f79
syedturabb <turabhaiderpk@gmail.com>
2025-09-20 01:31:25 -0700

    nested loops

--- iterations/one.js
+++ iterations/one.js
@@ -2,6 +2,22 @@
 
 for (let index = 0; index <10; index++) {
     const element = index
+    if(element==5){
+        console.log("5 numberss");
+        
+    }
     console.log(element);
+    
+}
+
+
+for (let i = 0; i < 10; i++) 
+    {
+        for (let index = 0; index < array.length; index++) {
+              console.log("inner loop");
+                          
+        }                           //nested loops
+    console.log("outer loop");
+    
     
 }
