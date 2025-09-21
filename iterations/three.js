59f177c3b7e1a2f3c25341484ba87c703f1a5b10
syedturabb <turabhaiderpk@gmail.com>
2025-09-21 01:48:39 -0700

    maps

--- iterations/three.js
+++ iterations/three.js
@@ -8,3 +8,36 @@ for (const val of arr) {
     console.log(val);
     
 }
+
+const greeting = "Hello"
+for (const greet of greeting) {
+    console.log(`each charis ${greet}`);
+    
+}
+
+
+// Maps   
+
+const map = new Map()
+map.set('pk', "pakistan")
+map.set('ir',"iran")
+
+
+console.log(map);
+
+for (const [key,value] of map) {
+    console.log(key , ':-', value);
+    
+}
+
+
+const myobject = {
+    'game1': 'nfs',
+    'game2': 'cod'
+}
+
+for (const [key,value] of myobject) {
+      console.log(key , ':-', value);
+}
+
+// maps are iteratable 
