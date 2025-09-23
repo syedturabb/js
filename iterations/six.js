9ae5e0b42a9864a85c83fe351a0cacac1ddd6fc1
syedturabb <turabhaiderpk@gmail.com>
2025-09-22 23:23:40 -0700

    filter use

--- iterations/six.js
+++ iterations/six.js
@@ -12,6 +12,24 @@ const mynum = [1,2,3,4,5,6,7,8,9]
 
 
 const newNum = mynum.filter((num) => num > 5 )  
+
+//   const newNum = mynum.filter((num) => { 
+//     return           num > 5 }
+//  )               
 //filter return a value , condtion is must
+
+// mynum.forEach((num) =>{
+//     if(num>4){
+//         newNum.push(num)
+//     }
+// })
+
+
+
+let userbooks = book.filter((bk) => bk.publish >= 2000)
+let userbooks = book.filter((bk) => {
+    return bk.genre === 'History' && bk.edition < 2000
+})
+
 
 
