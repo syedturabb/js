ef0b8d11ef64a5d393709f7dc739bf1ceacf4a77
syedturabb <turabhaiderpk@gmail.com>
2025-09-22 23:29:43 -0700

    map

--- iterations/seven.js
+++ iterations/seven.js
@@ -1,5 +1,13 @@
 const myNum = [1,2,3,4,5,6,7,8]
 
-const newnum=myNum.map( (num) => num+10)
+// const newnum=myNum.map( (num) => num+10)
+
+ /// chaining
+const newNum = myNum
+.map((num) => num*10)                 
+.map((num) => num+1) 
+.filter((num) => num>=40)                        //in chaining , seconds gets value from firts
 console.log(newnum);
+
+
 
