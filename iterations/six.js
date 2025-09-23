2023f87c06a6908e1ac99de47d9a271803802fee
syedturabb <turabhaiderpk@gmail.com>
2025-09-22 23:14:19 -0700

    filter func

---
+++ iterations/six.js
@@ -0,0 +1,17 @@
+const coding = ["js","python", "R","java"]
+
+//for each doesnot return value   
+
+const val = coding.forEach((item)=>{
+    return item
+})
+console.log(val);
+
+
+const mynum = [1,2,3,4,5,6,7,8,9]
+
+
+const newNum = mynum.filter((num) => num > 5 )  
+//filter return a value , condtion is must
+
+
